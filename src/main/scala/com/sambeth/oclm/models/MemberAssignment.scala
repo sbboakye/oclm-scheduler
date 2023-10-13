7ad7ebf491cfea740424bbf4be71a26bf2f152ec
sambeth <sambethslim@gmail.com>
2023-10-13 18:41:33 +0000

    use multiple apply methods in memberassignment to achieve difference results

--- src/main/scala/com/sambeth/oclm/models/MemberAssignment.scala
+++ src/main/scala/com/sambeth/oclm/models/MemberAssignment.scala
@@ -4,19 +4,22 @@ import Gender.*
 import Member.*
 
 enum MemberAssignment {
-  case BibleReading[G](student: Member[G])
+  case BibleReading(student: Member[Male.type])
   case InitialCall[G](publisher: Member[G], householder: Member[G])
   case ReturnVisit[G](publisher: Member[G], householder: Member[G])
   case BibleStudy[G](publisher: Member[G], householder: Member[G])
-  case FiveMinutesTalk[G](student: Member[G])
+  case FiveMinutesTalk(student: Member[Male.type])
 }
 
 object MemberAssignment {
   def apply[G <: Gender](nameOfAssignment: String, owner: Member[G], support: Option[Member[G]]): MemberAssignment =
     nameOfAssignment match
-      case "BibleReading" => BibleReading(owner)
       case "InitialCall" => InitialCall(owner, support.get)
       case "ReturnVisit" => ReturnVisit(owner, support.get)
       case "BibleStudy" => BibleStudy(owner, support.get)
+
+  def apply(nameOfAssignment: String, owner: Member[Male.type]): MemberAssignment =
+    nameOfAssignment match
+      case "BibleReading" => BibleReading(owner)
       case "FiveMinutesTalk" => FiveMinutesTalk(owner)
 }
