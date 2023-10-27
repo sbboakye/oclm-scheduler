9a9eb8e71ba73d6d0a68760bb3d5dcc38f143caf
sambeth <sambethslim@gmail.com>
2023-10-27 00:03:40 +0000

    add config typesafe library & workbook model

--- src/main/scala/MainApp.scala
+++ src/main/scala/MainApp.scala
@@ -1,39 +1,21 @@
+import cats.data.Kleisli
 import com.sambeth.oclm.models.gender.Gender
 import com.sambeth.oclm.models.gender.Gender.*
 import com.sambeth.oclm.models.appointedman.AppointedMan
 import com.sambeth.oclm.models.appointedman.AppointedManResponsibility.*
-
 import com.sambeth.oclm.models.student.Student
+import com.sambeth.oclm.models.workbook.WorkBook
+import com.typesafe.config.{Config, ConfigFactory}
 
 
 object MainApp extends App {
 
-  val elder = AppointedMan[Elder.type]("Samuel Baafi Boakye")
-  val ms = AppointedMan[MinisterialServant.type]("Samuel Acquah")
-  val maleStudent = Student[Male.type]("Tony Gyan")
-  val maleStudent2 = Student[Male.type]("Real Gyan")
-  val femaleStudent = Student[Female.type]("Casey Gyan")
-  val femaleStudent2 = Student[Female.type]("Esther Gyan")
-  println(elder)
-  println(ms)
-  println(maleStudent)
-  println(femaleStudent)
+  case class OCLMConfig(config: Config)
+  val oclmConfig: Config = ConfigFactory.load("oclm.conf")
 
-//  println(maleStudent.assignBibleStudy(femaleStudent2))
-  println(maleStudent.assignBibleStudy(maleStudent2))
-  println(maleStudent.assignFiveMinutesTalk)
-  println(maleStudent.assignFiveMinutesTalk)
-//  println(elder.assign)
-//  println(ms.assign)
+  trait OCLM
+  object OCLM {
+    val fromOCLMConfig: Kleisli[List, OCLMConfig, WorkBook] = ???
+  }
 
-//  println(MemberAssignment.assign())
-//  println(InitialCall[Male.type].assign(maleStudent, maleStudent2))
-//  MemberAssignment(_, maleStudent, femaleStudent2)
-//  MemberAssignment("InitialCall", femaleStudent, femaleStudent2)
-//  MemberAssignment("InitialCall", femaleStudent, maleStudent2)
-
-
-//  println()
-//  println(maleStudent.pair(maleStudent2))
-//  println(femaleStudent.pair(femaleStudent2))
 }
